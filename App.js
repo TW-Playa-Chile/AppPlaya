24267cf44d9dec218b714dcc5aba081beb313171
Alfredo Segundo <asegundo@thoughtworks.com>
2017-08-31 13:59:38 -0300

    Added save button and text input field.

--- App.js
+++ App.js
@@ -1,23 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {Alert, AsyncStorage, Button, TextInput, View} from "react-native";
+
+const bind = (context) => (...methods) => (methods.forEach(method => context[method] = context[method].bind(context)));
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
-}
+    constructor(props) {
+        super(props);
+        this.state = {text: ''};
+        bind(this)('_onPressSave', '_onPressClear');
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    async _onPressSave() {
+        try {
+            let activityText = this.state.text;
+            await AsyncStorage.getItem('@ActivityStore:activities', (err, result) => {
+                let allActivities = [];
+                let storedActivities = JSON.parse(result);
+                if (storedActivities) {
+                    allActivities = storedActivities;
+                }
+                AsyncStorage.setItem('@ActivityStore:activities', JSON.stringify(allActivities.concat([activityText])));
+            });
+            await AsyncStorage.getItem('@ActivityStore:activities', (err, result) => {
+                Alert.alert('Registro exitoso', 'actividades: ' + result);
+            });
+        } catch (error) {
+            console.log(error);
+            Alert.alert('Error en el registro');
+        }
+        this.setState({text: ''});
+    }
+
+    async _onPressClear() {
+        await AsyncStorage.clear()
+            .then(Alert.alert('Cleared'))
+            .catch(error => Alert.alert('Error'));
+    }
+
+    render() {
+        return (
+            <View style={{flex: 1, justifyContent: 'center'}}>
+                <TextInput
+                    onChangeText={(text) => this.setState({text})}
+                    value={this.state.text}
+
+                />
+                <View style={{padding: 10}}>
+                    <Button
+                        onPress={this._onPressSave}
+                        title="Save"
+                        color="#841584"
+                        accessibilityLabel="Save new activity"
+                    />
+                </View>
+                <View style={{padding: 10}}>
+                    <Button
+                        onPress={this._onPressClear}
+                        title="Clear"
+                        color="#841584"
+                        accessibilityLabel="Remove all activity"
+                    />
+                </View>
+            </View>
+        );
+    }
+}
