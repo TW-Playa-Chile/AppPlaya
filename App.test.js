5895cf4f63d4208a77be4f41999940f2b5f6058d
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2017-09-15 13:33:15 -0300

    [Alvaro] Remove duplicated test with snapshot

--- App.test.js
+++ App.test.js
@@ -4,11 +4,6 @@ import App from './App';
 
 import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<App />).toJSON();
-  expect(rendered).toBeTruthy();
-});
-
 it('renders correctly', () => {
   const tree = renderer.create(
       <App />
