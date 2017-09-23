f9049bc8a9bb2049c7b31e30971c87f33fef127d
alvaroHernandez <alvaro.hernandezi@alumnos.usm.cl>
2017-09-23 18:21:04 -0300

    [Alvaro] Add test for text state when input is changed

--- App.test.js
+++ App.test.js
@@ -1,6 +1,8 @@
 import 'react-native';
 import React from 'react';
 import App from './App';
+import { shallow } from 'enzyme';
+import {TextInput} from 'react-native';
 
 import renderer from 'react-test-renderer';
 
@@ -10,4 +12,13 @@ it('renders correctly', () => {
   ).toJSON();
 
   expect(tree).toMatchSnapshot();
+});
+
+it('should change the activity text state when input is changed', () => {
+    const fakeActivity = 'Activity 1';
+    const wrapper = shallow(<App/>);
+
+    wrapper.find(TextInput).simulate('ChangeText',fakeActivity);
+
+    expect(wrapper.state('text')).toBe(fakeActivity);
 });
